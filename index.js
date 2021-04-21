1145a34fe622fa5360eb21be633f7f14f69232da
insookim0702 <insookim0702@gmail.com>
2021-04-21 22:26:46 +0900

    스크롤 상단 이동 버튼 기능 추가

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const ALLOW_START_OFFSETY_RATIO = 0.6;
+const ALLOW_END_OFFSETY_RATIO = 0.7;
+const body = document.body;
+const topAllow = document.querySelector('.top_allow');
+const INNER_HEIGHT = window.innerHeight;
+const WEB_PAGE_TOTAL_HEIGHT = body.scrollHeight;
+const ALLOW_START_OFFSETY = WEB_PAGE_TOTAL_HEIGHT * ALLOW_START_OFFSETY_RATIO;
+const ALLOW_END_OFFSETY = WEB_PAGE_TOTAL_HEIGHT * ALLOW_END_OFFSETY_RATIO;
+
 function init() {
     const currentJobPeriod = document.querySelectorAll('.currentJobPeriod');
     const totalCareer = document.querySelector('.totalCareer');
@@ -7,7 +16,6 @@ function init() {
     const brandiPeriod = 2;
     const diffMonth = currentDate.getMonth() - machbaseJobStartDate.getMonth() + 1;
     const diffYear = currentDate.getFullYear() - machbaseJobStartDate.getFullYear();
-    console.log(Math.floor((wisenutPeriod + brandiPeriod + diffMonth) / 12));
     totalCareer.innerText = `${Math.floor((wisenutPeriod + brandiPeriod + diffMonth) / 12) + diffYear}년 ${(wisenutPeriod + brandiPeriod + diffMonth) % 12}개월`;
 
     currentJobPeriod.forEach((aItem) => {
@@ -18,6 +26,27 @@ function init() {
         period += `${diffMonth}M`;
         aItem.innerText = period;
     });
+
+    window.addEventListener('scroll', () => {
+        scrollReact();
+    });
+
+    topAllow.addEventListener('click', () => {
+        window.scroll({
+            behavior: 'smooth',
+            left: 0,
+            top: body.offsetTop,
+        });
+    });
+}
+
+function scrollReact() {
+    if ((pageYOffset + INNER_HEIGHT) / WEB_PAGE_TOTAL_HEIGHT > ALLOW_START_OFFSETY_RATIO) {
+        topAllow.style.display = 'block';
+        topAllow.style.opacity = (pageYOffset + INNER_HEIGHT - ALLOW_END_OFFSETY) / (ALLOW_END_OFFSETY - ALLOW_START_OFFSETY);
+    } else {
+        topAllow.style.display = 'none';
+    }
 }
 
 init();
