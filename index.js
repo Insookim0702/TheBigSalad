03a47c26cd8c270a74a0f7a3a6e6c07c06d1dae7
insookim0702 <insookim0702@gmail.com>
2021-04-24 17:45:02 +0900

    add function of scroll react for contens

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+const body = document.body;
+const currentJobPeriod = document.querySelectorAll('.currentJobPeriod');
+const totalCareer = document.querySelector('.totalCareer');
+const topAllow = document.querySelector('.top_allow');
+const CONTENTS = document.querySelector('.contents');
+const SECTION = document.querySelectorAll('section');
+const LITTLE_TITLE = document.querySelectorAll('.little_title');
+
 const ALLOW_START_OFFSETY_RATIO = 0.6;
 const ALLOW_END_OFFSETY_RATIO = 0.7;
-const body = document.body;
-const topAllow = document.querySelector('.top_allow');
+
 const INNER_HEIGHT = window.innerHeight;
 const WEB_PAGE_TOTAL_HEIGHT = body.scrollHeight;
 const ALLOW_START_OFFSETY = WEB_PAGE_TOTAL_HEIGHT * ALLOW_START_OFFSETY_RATIO;
 const ALLOW_END_OFFSETY = WEB_PAGE_TOTAL_HEIGHT * ALLOW_END_OFFSETY_RATIO;
 
+const SECTION1_START_Y_PX = SECTION[0].clientHeight + 120;
+const SECTION2_START_Y_PX = SECTION1_START_Y_PX + SECTION[1].clientHeight + 100;
+const SECTION3_START_Y_PX = SECTION2_START_Y_PX + SECTION[2].clientHeight + 100;
+const SECTION4_START_Y_PX = SECTION3_START_Y_PX + SECTION[3].clientHeight + 120;
+const SECTION_START_Y_PX = [SECTION1_START_Y_PX, SECTION2_START_Y_PX, SECTION3_START_Y_PX, SECTION4_START_Y_PX, WEB_PAGE_TOTAL_HEIGHT];
+
 function init() {
-    const currentJobPeriod = document.querySelectorAll('.currentJobPeriod');
-    const totalCareer = document.querySelector('.totalCareer');
     const currentDate = new Date();
     const machbaseJobStartDate = new Date(2021, 1, 25);
     const wisenutPeriod = 15;
@@ -29,6 +40,7 @@ function init() {
 
     window.addEventListener('scroll', () => {
         scrollReact();
+        scrollReactContents();
     });
 
     topAllow.addEventListener('click', () => {
@@ -37,6 +49,63 @@ function init() {
             left: 0,
             top: body.offsetTop,
         });
+    });
+
+    LITTLE_TITLE.forEach((element, index) => {
+        element.addEventListener('click', () => {
+            console.log(index);
+            console.log(SECTION_START_Y_PX[index]);
+            scrollTo(0, SECTION_START_Y_PX[index] + 10);
+        });
+    });
+}
+
+function scrollReactContents() {
+    if (pageYOffset < 380) {
+        CONTENTS.style.position = 'absolute';
+        CONTENTS.style.top = '0px';
+        LITTLE_TITLE.forEach((element, index) => {
+            element.style.color = '#bdc3c7';
+            element.style.transform = 'scale(1)';
+            element.style.marginRight = '0px';
+        });
+    } else {
+        CONTENTS.style.position = 'fixed';
+        CONTENTS.style.top = '100px';
+        if (SECTION1_START_Y_PX < pageYOffset) {
+            contentStyle(0);
+            // Work Experience
+        }
+        if (SECTION2_START_Y_PX < pageYOffset) {
+            contentStyle(1);
+            // My Salad Project
+        }
+        if (SECTION3_START_Y_PX < pageYOffset) {
+            contentStyle(2);
+            // Alone Study
+        }
+        if (SECTION4_START_Y_PX < pageYOffset) {
+            contentStyle(3);
+            // Skill
+        }
+        if (WEB_PAGE_TOTAL_HEIGHT < pageYOffset + INNER_HEIGHT) {
+            contentStyle(4);
+            // Contact
+        }
+    }
+}
+
+function contentStyle(idx) {
+    LITTLE_TITLE.forEach((element, index) => {
+        if (index === idx) {
+            element.style.color = 'black';
+            element.style.transform = 'scale(1.2)';
+            element.style.marginRight = '14px';
+        } else {
+            element.style.color = '#bdc3c7';
+            element.style.transform = 'scale(1)';
+            element.style.marginRight = '0px';
+        }
     });
 }
 
